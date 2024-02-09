242e01d0d5941a01b48ce8e45cd92581ad9d1fa1
RishiGupta-05 <rutkars1@gmail.com>
2024-02-09 22:58:35 +0530

    Initial Commit

---
+++ djangoapp/core/views.py
@@ -0,0 +1,35 @@
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+# Create your views here.
+def frontpage(request):
+    return render(request,'C:/Users/rutka/Desktop/Projects/djangochat_env/djangoapp/core/templates/core/frontpage.html')
+
+'''def signup(request):
+
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+
+            login(request, user)
+
+            return redirect('frontpage')
+        
+        else:
+            form = SignUpForm()
+
+        return render(request, 'C:/Users/rutka/Desktop/Projects/djangochat_env/djangoapp/core/templates/core/signup.html', {'form':form})'''
+    
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('frontpage')
+    else:  # Handle the GET request here
+        form = SignUpForm()
+
+    return render(request, 'C:/Users/rutka/Desktop/Projects/djangochat_env/djangoapp/core/templates/core/signup.html', {'form': form})
